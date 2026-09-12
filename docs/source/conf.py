ec8230b0b2bb768ba1e20d1912274a3452e256c7
oAI Chat for Thomas <thomasleaf+chat@gmail.com>
2026-09-11 22:24:14 -0700

    Configure Sphinx for Zero documentation

---
+++ docs/source/conf.py
@@ -0,0 +1,12 @@
+project = "django-trusts-zero"
+copyright = "2015-2026, BeeDesk, Inc."
+author = "BeeDesk, Inc."
+version = "1.0.0.dev0"
+release = version
+
+extensions = []
+source_suffix = ".rst"
+master_doc = "index"
+exclude_patterns = []
+html_theme = "alabaster"
+htmlhelp_basename = "django-trusts-zerodoc"
